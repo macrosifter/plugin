77d471acc94f318958e6f4a1f04ffed478cb7cef
M.K. Safi <msafi@msafi.com>
2023-06-06 12:55:32 -0700

    figure out ssl flask version stuff

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,81 @@
-from flask import Flask
+import os
+import plaid
+import time
+import json
+
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.api import plaid_api
 
 app = Flask(__name__)
+load_dotenv()
+
+def empty_to_none(field):
+    value = os.getenv(field)
+    if value is None or len(value) == 0:
+        return None
+    return value
+
+PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
+PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
+PLAID_REDIRECT_URI = empty_to_none('PLAID_REDIRECT_URI')
+PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
+PLAID_SECRET = os.getenv('PLAID_SECRET')
+
+host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'sandbox':
+    host = plaid.Environment.Sandbox
+
+if PLAID_ENV == 'development':
+    host = plaid.Environment.Development
+
+if PLAID_ENV == 'production':
+    host = plaid.Environment.Production
+
+configuration = plaid.Configuration(
+    host=host,
+    api_key={
+        'clientId': PLAID_CLIENT_ID,
+        'secret': PLAID_SECRET,
+        'plaidVersion': '2020-09-14'
+    }
+)
+
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
+
+products = []
+for product in PLAID_PRODUCTS:
+    products.append(Products(product))
 
 @app.route("/")
 def index():
-    return "Hello World!zsssss"
+    return "The MacroSifter API is running!"
+
+@app.route('/create_link_token', methods=['GET'])
+def root():
+    try:
+        request = LinkTokenCreateRequest(
+            products=products,
+            client_name="Plaid Quickstart",
+            country_codes=list(map(lambda x: CountryCode(x), PLAID_COUNTRY_CODES)),
+            language='en',
+            user=LinkTokenCreateRequestUser(
+                client_user_id=str(time.time())
+            )
+        )
+        if PLAID_REDIRECT_URI!=None:
+            request['redirect_uri']=PLAID_REDIRECT_URI
+
+        #  create link token
+        response = client.link_token_create(request)
+        return jsonify(response.to_dict())
+
+    except plaid.ApiException as e:
+        return json.loads(e.body)
